63aa6fd3c0e47fa550938398f9cccd422997b3ff
junqiangmai <74021314+junqiangmai@users.noreply.github.com>
2020-11-11 17:34:26 -0500

    hooked up api to temp

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 window.addEventListener('load', () => {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector(".temperature-description");
-  let temperatureDegree = document.querySelector(".temperature-degree");
-  let locationTimezone = document.querySelector(".location-timezone");
+  let temperatureDescription = document.querySelector(".weather");
+  let temperatureDegree = document.querySelector(".temperature");
+  let maxtemp = document.querySelector(".maxtemp");
+  let mintemp = document.querySelector(".mintemp");
+  let iconImage = document.querySelector(".images img");
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition
     (position=>{
@@ -18,17 +20,21 @@ window.addEventListener('load', () => {
            })
            .then(data =>{
              console.log(data)
-             const {temp_f, condition, feelslike_f, humidity, vis_miles, wind_mph}= data.current;
+             const {temp_f, feelslike_f, humidity, vis_miles, wind_mph}= data.current;
              const {name,} = data.location
-             const {maxtemp_f, mintemp_f} = data.forcast.forecastday
+             const {text, icon} = data.current.condition
+             const {maxtemp_f, mintemp_f} = data.forecast.forecastday[0].day
              //Set DOM Elements from the API
-             //temperatureDegree.textContent = Temperature;
-             //temperatureDescription.textContent = Condition;
-             //locationTimezone.textContent = data.timezone;
+             temperatureDegree.textContent = temp_f + "°F";
+             temperatureDescription.textContent = text;
+             maxtemp.textContent = maxtemp_f + "°F";
+             mintemp.textContent = mintemp_f + "°F";
+             iconImage.src = icon;
+
              console.log("City", name);
              console.log("Day", "Mon");
              console.log("Time", "3:00 pm");
-             console.log("Condition", condition);
+             console.log("Condition", text);
              console.log("Temp", temp_f + "°F");
              console.log("Feels Like", feelslike_f + "°F")
              console.log("Humidity", humidity + "%")
@@ -36,10 +42,7 @@ window.addEventListener('load', () => {
              console.log("Wind", wind_mph + " mph")
              console.log("Min_Temp", mintemp_f + "°F");
              console.log("Max_Temp", maxtemp_f + "°F");
-             console.log(data)
        });
     });
   }
-
-  //function setIcons(icon, iconid)
 });
